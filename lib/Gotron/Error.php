1c4acebeb1c9447f2372e741c5bacd7d87e969da
Scott Bader <sbader2@gmail.com>
2012-04-02 16:28:54 -0400

    Updated Router, added namespace to Error calls

--- lib/Gotron/Error.php
+++ lib/Gotron/Error.php
@@ -9,24 +9,24 @@ namespace Gotron;
  */
 class Error {
 
-    public static function error_401() {
+    public static function error_401($namespace) {
         Header::set("HTTP/1.0 401 Unauthorized");
-        Router::perform_controller_action('Error', 'error_page', null, array('page' => 401) );
+        Router::perform_controller_action('Error', 'error_page', null, array('page' => 401), $namespace);
     }
 
-    public static function error_403() {
+    public static function error_403($namespace) {
         Header::set("HTTP/1.0 403 Forbidden");
-        Router::perform_controller_action('Error', 'error_page' , null , array('page' => 403));
+        Router::perform_controller_action('Error', 'error_page' , null , array('page' => 403), $namespace);
     }
 
-    public static function error_404() {
+    public static function error_404($namespace) {
         Header::set("HTTP/1.0 404 Not Found");
-        Router::perform_controller_action('Error', 'error_page', null, array('page' => 404));
+        Router::perform_controller_action('Error', 'error_page', null, array('page' => 404), $namespace);
     }
 
-    public static function error_500() {
+    public static function error_500($namespace) {
         Header::set("HTTP/1.0 500 Internal Server Error");
-        Router::perform_controller_action('Error', 'error_page', null, array('page' => 500));
+        Router::perform_controller_action('Error', 'error_page', null, array('page' => 500), $namespace);
     }
 
 }
