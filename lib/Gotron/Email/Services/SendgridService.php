61cde6d95d759760e35a47c197beedbcedbb17d5
Scott Bader <sbader2@gmail.com>
2012-04-11 16:53:52 -0400

    Updated SendgridService to use SmtpApiHeader properly

--- lib/Gotron/Email/Services/SendgridService.php
+++ lib/Gotron/Email/Services/SendgridService.php
@@ -2,7 +2,11 @@
 
 namespace Gotron\Email\Services;
 
-use Swift_Message;
+use Swift_Message,
+    Swift_Mailer,
+    SmtpApiHeader,
+    Swift_SmtpTransport,
+    Gotron\Config;
 
 require_once __DIR__ . '/../../../vendor/Swift/swift_init.php';
 
@@ -18,8 +22,9 @@ class SendgridService extends EmailService
     const SERVER = "SENDGRID_SERVER";
     const PORT = "SENDGRID_PORT";
 
-	public function send($email)
-	{
+    protected $swift = null;
+
+	public function send($email) {
 	    $response = $this->send_request($email);
 	    if($response === 1)
 	        return true;
@@ -47,15 +52,27 @@ class SendgridService extends EmailService
 	 */
 	public function build_message($email)
 	{
-	    $message = new \Swift_Message;
+	    $message = new Swift_Message;
 	    $message->setReplyTo($email->reply_to);
 	    $message->setFrom($email->from);
         $message->setTo($email->to);
         $message->setSubject($email->subject);
 		$message->setBody($email->html_content, 'text/html');
 		$message->addPart($email->text_content, 'text/plain');
+
+        $headers = $message->getHeaders();
+        $headers->addTextHeader('X-SMTPAPI', $this->build_headers($email));
+
 		return $message;
 	}
+
+    public function build_headers($email) {
+        $api_header = new SmtpApiHeader;
+        $site_type = defined('SITE_TYPE') ? SITE_TYPE : "development";
+        $category = $site_type . '-' . $email->type;
+        $api_header->setCategory($category);
+        return $api_header->asJSON();
+    }
 	
 	/**
 	 * Gets the SMTP connection
@@ -64,16 +81,23 @@ class SendgridService extends EmailService
 	 */
 	private function get_sendgrid_connection()
 	{
-	    if(!is_null($this->swift)){
+	    if($this->swift instanceof Swift_Mailer){
 	        return $this->swift;
         }
-        else{
-            $this->emailHeader = new \SmtpApiHeader();
-            $this->transport = \Swift_SmtpTransport::newInstance(self::SERVER, self::PORT);
+        else {
+            $config = Config::instance();
+            $config = $config['sendgrid'];
+
+            $server = isset($config['server']) ? $config['server'] : self::SERVER;
+            $port = isset($config['port']) ? $config['port'] : self::PORT;
+            $user = isset($config['user']) ? $config['user'] : self::USER;
+            $password = isset($config['password']) ? $config['password'] : self::PASSWORD;
+
+            $this->transport = Swift_SmtpTransport::newInstance($server, $port);
             $this->transport->setTimeout(15);
-            $this->transport->setUsername(self::USER);
-            $this->transport->setPassword(self::PASSWORD);
-            return $this->swift = \Swift_Mailer::newInstance($this->transport);
+            $this->transport->setUsername($user);
+            $this->transport->setPassword($password);
+            return $this->swift = Swift_Mailer::newInstance($this->transport);
         }
 	}
 	
