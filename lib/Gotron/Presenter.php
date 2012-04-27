b3606da8c15f38407826c46ce54e9692e645ae69
sbader <sbader2@gmail.com>
2012-04-27 15:23:29 -0400

    Swapping Presenter to_array/as_array

--- lib/Gotron/Presenter.php
+++ lib/Gotron/Presenter.php
@@ -4,9 +4,9 @@ namespace Gotron;
 
 class Presenter extends Singleton {
 
-    public static function as_array($model) {
+    public static function to_array($model) {
         $instance = new static($model);
-        return $instance->to_array();
+        return $instance->as_array();
     }
 
 }
