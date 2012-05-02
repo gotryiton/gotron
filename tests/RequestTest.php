5056cfa8eb48f8ea07c39b7e73bbe64c087c38e4
sbader <sbader2@gmail.com>
2012-05-02 14:06:16 -0400

    Updated RequestTest

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -2,30 +2,37 @@
 
 namespace TestApp;
 
-use Gotron\Request;
+use Gotron\Dispatch\Request;
 
 class RequestTest extends UnitTest {
 
     public function test_load_content_type_and_version() {
-        $_SERVER['HTTP_ACCEPT'] = "application/v3-json";
         $request = new Request;
-        $request->load_content_type_and_version();
+        $request->accept_header = "application/v3-json";
+        $request->load_content_type_and_version(array());
         $this->assertEquals(3, $request->version);
         $this->assertEquals("application/json", $request->content_type);
     }
 
     public function test_load_content_type_and_version_no_accept() {
-        unset($_SERVER['HTTP_ACCEPT']);
         $request = new Request;
-        $request->load_content_type_and_version();
+        $request->load_content_type_and_version(array());
         $this->assertEquals(4, $request->version);
         $this->assertEquals("text/html", $request->content_type);
     }
 
-    public function test_build_request() {
-        $_SERVER['HTTP_ACCEPT'] = "application/v5-json";
+    public function test_load_content_type_and_version_content_type_set() {
         $request = new Request;
-        $request->load_content_type_and_version();
+        $request->content_type = "application/json";
+        $request->load_content_type_and_version(array());
+        $this->assertEquals(4, $request->version);
+        $this->assertEquals("application/json", $request->content_type);
+    }
+
+    public function test_build_request() {
+        $request = new Request;
+        $request->accept_header = "application/v5-json";
+        $request->load_content_type_and_version(array());
         $this->assertEquals(5, $request->version);
         $this->assertEquals("application/json", $request->content_type);
     }
