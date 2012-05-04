950bee8920b3058204a1332656ddd3520148fbe3
Simon Holroyd <sholroyd@gmail.com>
2012-05-03 20:19:25 -0400

    reworking Presenter class to have a single constructor

--- lib/Gotron/Presenter.php
+++ lib/Gotron/Presenter.php
@@ -4,6 +4,15 @@ namespace Gotron;
 
 class Presenter {
 
+	public $options = array();
+
+    public function __construct($model, $options = array()) {
+        $this->model = $model;
+        foreach ($options as $key => $value){
+        	$this->$key = $value;
+        }        
+    }
+
     public static function to_array($model, $options = array()) {
         $instance = new static($model, $options);
         return $instance->as_array();
