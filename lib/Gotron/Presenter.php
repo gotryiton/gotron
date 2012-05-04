9d0ec4cfc84ba7bff9a608db9e477184de4dbe0d
Simon Holroyd <sholroyd@gmail.com>
2012-05-04 10:06:44 -0400

    Updating presenter

--- lib/Gotron/Presenter.php
+++ lib/Gotron/Presenter.php
@@ -4,19 +4,56 @@ namespace Gotron;
 
 class Presenter {
 
-	public $options = array();
+	/**
+	 * Array of cached singleton objects.
+	 *
+	 * @var array
+	 */
+	private static $instances = array();
 
-    public function __construct($model, $options = array()) {
-        $this->model = $model;
-        foreach ($options as $key => $value){
-        	$this->$key = $value;
-        }        
+	public $u = null;
+
+
+	/**
+	 * Static method for instantiating a singleton object.
+	 *
+	 * @return object
+	 */
+	final public static function instance() {
+		$class_name = get_called_class();
+
+		if (!isset(self::$instances[$class_name]))
+			self::$instances[$class_name] = new $class_name;
+
+		return self::$instances[$class_name];
+	}
+
+	/**
+	 * Singleton objects should not be cloned.
+	 *
+	 * @return void
+	 */
+	final private function __clone() {}
+
+
+    public static function to_array($model) {
+    	$root_presenter = Presenter::instance();
+    	
+    	$presenter = self::instance();
+
+    	$presenter->u = $root_presenter->u;
+
+    	return $presenter->as_array($model);
+
+    	      
     }
 
-    public static function to_array($model, $options = array()) {
-        $instance = new static($model, $options);
-        return $instance->as_array();
+    public static function inherit_user($u){
+    	$p = self::instance();
+    	$p->u = $u;
     }
+
+   
 
 }
 
