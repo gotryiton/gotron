0056de7837444bf6129fe73e4ce7268d2728e2b7
sbader <sb@scottbader.org>
2012-07-18 18:51:38 -0400

    testing subject prefix

--- tests/Email/EmailTest.php
+++ tests/Email/EmailTest.php
@@ -94,6 +94,29 @@ class EmailTest extends UnitTest {
         $this->assertEquals("This is a test email Hello", $email->text_content);
     }
 
+    public function testCreateEmailWithSubjectPrefix() {
+        $options = array(
+            'to' => 'Test Person <test@gotryiton.com>',
+            'reply_to' => 'Test GTIO <reply@gotryiton.com>',
+            'data' => array("message" => "Hello"),
+            'view_path' => __DIR__ . "/../helpers/emails/",
+            'layout' => 'override_email',
+            'subject_prefix' => 'BLAM'
+        );
+
+        $email_type = "test_email";
+
+        $email = Email::create($email_type, $options);
+
+        $this->assertEquals($email_type, $email->type);
+        $this->assertEquals("BLAM Overriding the subject", $email->subject);
+        $this->assertEquals($options['to'], $email->to);
+        $this->assertEquals($options['reply_to'], $email->reply_to);
+        $this->assertEquals($options['data'], $email->data);
+        $this->assertEquals("This is the start of a second test email layout\n<div>This is a test email</div>\n<div>Hello</div> \nThis is the end of a second test email layout", $email->html_content);
+        $this->assertEquals("This is the start of a second test email layout This is a test email\nHello This is the end of a second test email layout", $email->text_content);
+    }
+
 }
 
 ?>
