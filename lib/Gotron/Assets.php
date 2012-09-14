17cec885b052b3f9bdd22992e63a31a0b2074010
sbader <sb@scottbader.org>
2012-09-14 17:36:21 -0400

    Updated assets to use hash for caching, added images to assets

--- lib/Gotron/Assets.php
+++ lib/Gotron/Assets.php
@@ -5,21 +5,45 @@ namespace Gotron;
 class Assets {
 
     public static function javascript($object) {
-        if($path = Config::get('assets.js_location', true)) {
+        return static::get_filename($object, 'js');
+    }
+
+    public static function css($object) {
+        return static::get_filename($object, 'css');
+    }
+
+    public static function image($object) {
+        $file_info = pathinfo($object);
+        return static::get_filename($file_info['filename'], $file_info['extension'], 'images');
+    }
+
+    public static function get_filename($object, $extension, $prefix = null) {
+        $prefix = is_null($prefix) ? $extension : $prefix;
+        if($path = Config::get("assets.{$prefix}_location", true)) {
+            if (Config::get('assets.hashed', true)) {
+                $object = static::hash_object($object, $extension, $prefix);
+            }
+            $object = $object . ".{$extension}";
+
             return file_join($path, $object);
         }
         else {
-            return "/assets/javascripts/$object";
+            return "/assets/{$prefix}/{$object}.{$extension}";
         }
     }
-    
-    public static function css($object) {
-        if($path = Config::get('assets.css_location', true)) {
-            return file_join($path, $object);
+
+    public static function hash_object($object, $extension, $prefix = null) {
+        $prefix = is_null($prefix) ? $extension : $prefix;
+        $config = Config::instance();
+        if (!($dictionary = $config->get('assets_dictionary', true))) {
+            $dictionary = file_get_contents(file_join(Config::get('root_directory'), '../../shared/assets.json'));
+            $dictionary = json_decode($dictionary, true);
+            $config->set('assets_dictionary', $dictionary);
         }
-        else {
-            return "/assets/css/$object";
-        }
+
+        $sha =  $dictionary[$prefix]["{$object}.{$extension}"];
+
+        return "{$object}_{$sha}";
     }
     
 }
