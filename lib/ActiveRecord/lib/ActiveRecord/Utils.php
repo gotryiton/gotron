66c0fbae88beeeede965cf7d2b0a85e381f7a7b9
sbader <sb@scottbader.org>
2012-11-29 09:50:59 -0500

    added camelize and uncamelize methods used in ActiveRecord\Model

--- lib/ActiveRecord/lib/ActiveRecord/Utils.php
+++ lib/ActiveRecord/lib/ActiveRecord/Utils.php
@@ -374,5 +374,25 @@ class Utils
 	{
 		return preg_replace("/$char+/",$char,$string);
 	}
+
+    public static function camelize($string, $pascalCase = false) {
+        $string = str_replace(array('-', '_'), ' ', $string);
+        $string = ucwords($string);
+        $string = str_replace(' ', '', $string);
+
+        if(!$pascalCase) {
+            return lcfirst($string);
+        }
+
+        return $string;
+    }
+
+    public static function uncamelize($input) {
+        return preg_replace(
+            '/(^|[a-z])([A-Z0-9])/e',
+            'strtolower(strlen("\\1") ? "\\1_\\2" : "\\2")',
+            $input
+        );
+    }
 };
 ?>
