ebc8e81f5b0904598d03c5797e85aadc59c1a14b
sbader <sb@scottbader.org>
2012-12-28 12:57:27 -0500

    adding ErrorController and views

---
+++ lib/Gotron/ApplicationTemplates/app/views/error/404.php
@@ -0,0 +1,2 @@
+<h2>404</h2>
+<p>Sorry, that page does not exist</p>
---
+++ lib/Gotron/ApplicationTemplates/app/views/error/maintenance.php
@@ -0,0 +1,5 @@
+<?php
+$title = 'Maintenance';
+?>
+<h2>Maintenance.</h2>
+<p>We're currently performing maintenance. We'll be back soon.</p>
